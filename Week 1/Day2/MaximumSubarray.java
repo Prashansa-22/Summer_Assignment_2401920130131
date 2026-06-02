7a9dc1c8477604b47547220818b10ee37f8ac499
Prashansa Varshney <prashansavarshney91@gmail.com>
2026-06-02 17:02:23 +0530

    Create MaximumSubarray.java

---
+++ Week 1/Day2/MaximumSubarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxsum=nums[0];
+        int currsum=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(currsum<0){
+                currsum=nums[i];
+            }else{
+                currsum=currsum+nums[i];
+            }
+            if(currsum>maxsum){
+                maxsum=currsum;
+            }
+        }
+        return maxsum;
+    }
+}
