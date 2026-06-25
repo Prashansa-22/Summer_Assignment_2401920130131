b9ed476bb20a1e76ef4420baf7ffca28eee68412
Prashansa Varshney <prashansavarshney91@gmail.com>
2026-06-25 18:02:10 +0530

    Create BinaryTreeMaxPathSum.java

---
+++ Week 4/Day 4/BinaryTreeMaxPathSum.java
@@ -0,0 +1,16 @@
+class Solution {
+    int maxSum = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        helper(root);
+        return maxSum;
+    }
+    private int helper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = Math.max(0, helper(root.left));
+        int right = Math.max(0, helper(root.right));
+        maxSum = Math.max(maxSum, root.val + left + right);
+        return root.val + Math.max(left, right);
+    }
+}
